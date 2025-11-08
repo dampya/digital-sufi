9fce33b68fcd28017dd06996da328b4fb5283d5b
Omi0604 <omdighe0604@gmail.com>
2025-11-08 12:29:58 +0000

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  import Landing from './components/Landing.vue'
+</script>
+
+<template>
+  <Landing />
+</template>
+
---
+++ src/components/Landing.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Hi</h1>
+</template>
+
+<style src="./../styles/landing/landing.css"></style>
+
