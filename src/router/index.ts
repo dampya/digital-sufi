64a8951c3a71113c03636272adaeb9bf412e50d7
Omi0604 <omdighe0604@gmail.com>
2025-12-30 02:46:46 +0000

    added tools page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Main from './../components/main/Main.vue'
 import Games from '../components/games/Games.vue'
 import Legal from './../components/main/Legal.vue'
+import Tools from './../components/tools/Tools.vue'
 import Scoundrel from './../components/games/scoundrel/Scoundrel.vue'
 import Snake from './../components/games/snake/Snake.vue'
 import Minesweeper from './../components/games/minesweeper/Minesweeper.vue'
@@ -11,6 +12,7 @@ const routes= [
   { path: '/', name: 'main', component: Main },
   { path: '/legal', name: 'legal', component: Legal },
   { path: '/games', name: 'games', component: Games },
+  { path: '/tools', name: 'tools', component: Tools },
   { path: '/games/scoundrel', name: 'scoundrel', component: Scoundrel},
   { path: '/games/snake', name: 'snake', component: Snake },
   { path: '/games/minesweeper', name: 'minesweeper', component: Minesweeper},
